c13f3beffa8f13170113b17c5e4b7a7c2f9d181d
Ayden Son <65970555+yougottawork@users.noreply.github.com>
2022-04-07 07:08:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 
+# create a new CSV file
 # with the file we created, score each comment with the guidelines below
 # After when we're done with scoring, we can sort the comments in decreasing order
 
@@ -11,6 +12,14 @@ useful_wordfreq = {}
 funny_wordfreq = {}
 cool_wordfreq = {}
 
+extremewords = ['very', 'too', 'extremely', 'really','totally', 'furious',
+    'awful','terrible','horrible','filthy','wonderful','fantastic','excellent',
+        'fascinating','gorgeous','terrifying','disgusting','best','worst','rude',
+        'fuck','fucking','fucked','freaking','so','dick','ass','absolutely','absolute','shit',
+            'trash','omg','incredible','unbelievable','incredibly','unbelievably',
+                'wow','phenomenal','awesome','definitely','never','ever','greatest',
+                    'most','least','always','horrendous','must']
+
 #never, too, very, --> extreme word list
     # score each of those words
         #never, -3 points, 
@@ -19,59 +28,90 @@ cool_wordfreq = {}
         # Ratings, the number of votes, etc
 count=0
 
-with open("review_yelp_sample.csv", encoding='utf-8') as f:
+# input = open("review_yelp.csv", encoding='utf-8')
+# output = open("review_output.csv", 'w', newline='', encoding='utf-8')
+
+# output.write(input.readline())
+
+# for line in input:
+#     if not line.lstrip().startswith("#"):
+#         output.write(line)
+
+# input.close()
+# output.close()
+
+with open("review_yelp.csv", encoding='utf-8') as f, open("reviews_weighted.txt", 'w', encoding='utf-8') as g:
     lines = f.readlines()
+    g.write("Stars\tUseful\tFunny\tCool\tReview\tExtreme_words_ratio\tnum_of_proper_nouns\tnum_of_capped_words\n")
     for i in range(1, len(lines)-1): # from 2nd line
+        weight=0
+        ratingweight=0 #if rate is 1 or 5, we weight it 3 . If rate is 2 or 4, we weight it 2. If rate is 3, we weight it 1.
+        capword=0
+        numword=0
+        propernouns=0 #number of proper nouns in the comment will be counted
+        extword=0
         if (lines[i][0].isdigit()) :
             useful_tags = int(lines[i][2])
             funny_tags = int(lines[i][4])
             cool_tags = int(lines[i][6])
             total_tags = useful_tags + funny_tags + cool_tags
-            if (total_tags > 0) :
-                wordlist2 = []
-                wordlist = lines[i][8:].split()
-                for word in wordlist:
-                    for character in word:
-                        if character in string.punctuation:
-                            word = word.replace(character,"")
-                            word = word.lower() # to lowercase the words
-                    wordlist2.append(word)
-                    # wordlist2 will have all the words of the comment
-                most_tags = max(useful_tags , funny_tags , cool_tags)
-                if (most_tags == useful_tags) :
-                    for word in wordlist2:
-                        if word not in stopwords:
-                            if word in useful_wordfreq: # word frequency is stored here
-                                useful_wordfreq[word] += 1
-                            else:
-                                useful_wordfreq[word] = 1
-                if (most_tags == funny_tags) :
-                    # do the same but for funny_tags category
-                    for word in wordlist2:
-                        if word not in stopwords:
-                            if word in funny_wordfreq: # word frequency is stored here
-                                funny_wordfreq[word] += 1
-                            else:
-                                funny_wordfreq[word] = 1
-                if (most_tags == cool_tags) :
-                    # do the same but for cool_tags category
-                    for word in wordlist2:
-                        if word not in stopwords:
-                            if word in cool_wordfreq: # word frequency is stored here
-                                cool_wordfreq[word] += 1
-                            else:
-                                cool_wordfreq[word] = 1    
+            #if (total_tags > 0) :
+            wordlist2 = []
+            wordlist = lines[i][8:].split()
+            for word in wordlist:
+                for character in word:
+                    if character in string.punctuation:
+                        word = word.replace(character,"")
+                wordlist2.append(word)
+            for word2 in wordlist2:
+                if (word2.isupper()):
+                    capword += 1
+            numword = len(wordlist2)
+            for word4 in wordlist2:
+                if word4.lower() in extremewords:
+                    extword += 1
+                # wordlist2 will have all the words of the comment
+            most_tags = max(useful_tags , funny_tags , cool_tags)
+            if (most_tags == useful_tags) :
+                for word in wordlist2:
+                    if word not in stopwords:
+                        if word in useful_wordfreq: # word frequency is stored here
+                            useful_wordfreq[word] += 1
+                        else:
+                            useful_wordfreq[word] = 1
+            if (most_tags == funny_tags) :
+                # do the same but for funny_tags category
+                for word in wordlist2:
+                    if word not in stopwords:
+                        if word in funny_wordfreq: # word frequency is stored here
+                            funny_wordfreq[word] += 1
+                        else:
+                            funny_wordfreq[word] = 1
+            if (most_tags == cool_tags) :
+                # do the same but for cool_tags category
+                for word in wordlist2:
+                    if word not in stopwords:
+                        if word in cool_wordfreq: # word frequency is stored here
+                            cool_wordfreq[word] += 1
+                        else:
+                            cool_wordfreq[word] = 1   
+            extreme_word_ratio = (extword/numword)
+            extreme_word_ratio = round(extreme_word_ratio, 3)
+            g.write(lines[i][0] + '\t' + lines[i][2] + '\t' + lines[i][4] + '\t' + lines[i][6] + '\t' + lines[i][8:].strip('\n') + '\t' + str(extreme_word_ratio) + '\t' + str(capword) + '\n') 
 
-sorted_useful = dict( sorted(useful_wordfreq.items(),key=lambda item: item[1],reverse=True))
-sorted_funny = dict( sorted(funny_wordfreq.items(),key=lambda item: item[1],reverse=True))
-sorted_cool = dict( sorted(cool_wordfreq.items(),key=lambda item: item[1],reverse=True))
 
-print(sorted_useful)
-print("\n")
-print(sorted_funny)
-print("\n")
-print(sorted_cool)
-print("\n")
+
+
+# sorted_useful = dict( sorted(useful_wordfreq.items(),key=lambda item: item[1],reverse=True))
+# sorted_funny = dict( sorted(funny_wordfreq.items(),key=lambda item: item[1],reverse=True))
+# sorted_cool = dict( sorted(cool_wordfreq.items(),key=lambda item: item[1],reverse=True))
+
+# print(sorted_useful)
+# print("\n")
+# print(sorted_funny)
+# print("\n")
+# print(sorted_cool)
+# print("\n")
                 
 # reference
     # https://rstudio-pubs-static.s3.amazonaws.com/117743_53a6a1da3c724e7b8d608c3df7e40fb6.html#/3
@@ -86,5 +126,3 @@ print("\n")
     #Restaurants, manager names, the name of the menu/cuisine, 
 
 #word net
-
-# uspe first program to get ne, w csv, which becomes data ffor next  programfilor\
